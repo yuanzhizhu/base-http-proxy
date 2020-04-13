4775988d614690ea04b5f501101086b10033c051
zhuzhiyuan <zhuzhiyuan@youzan.com>
2020-04-13 23:38:51 +0800

    feat: 支持连续两次proxy

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 const Koa = require("koa");
-const httpProxy = require("http-proxy");
 const bodyParser = require("koa-bodyparser");
-const streamify = require("stream-array");
+
+const mainStationProxy = require('./middleware/mainStationProxy');
+const youzanyunProxy = require('./middleware/youzanyunProxy');
 
 const app = new Koa();
-const proxy = httpProxy.createProxyServer();
 
 app.use(bodyParser());
-
-app.use(async ctx => {
-  await new Promise(() => {
-    proxy.web(ctx.req, ctx.res, {
-      changeOrigin: true,
-      target: "https://diy.youzanyun.com/",
-      buffer: ctx.request.rawBody ? streamify([ctx.request.rawBody]) : undefined
-    });
-  });
-});
+app.use(mainStationProxy);
+app.use(youzanyunProxy);
 
 console.log("启动成功");
 app.listen(80);
